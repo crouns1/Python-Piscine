9d5495e8f383eba7c53e963508f5c3a3650020fb
Jamal Ait Chdid <jait-chd@c2r2p10.1337.ma>
2025-04-29 21:18:55 +0100

    python piscine push 1st 2 ex

---
+++ Python_00_Starting/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Morocco!")
+ft_set = {"Hello", "Rabat!"}
+ft_dict = {"Hello" : "42 Rabat Campus!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
